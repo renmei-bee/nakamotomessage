96e45d5b9ced5e76db0c335c891bbcd62bb9d436
renmei-bee <renmei800@gmail.com>
2021-11-29 14:02:43 +0900

    アップロード

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+$(function () {
+
+    // $("main img").click(function () {
+    //     $("main p").css("color", "red")
+    // });
+    $(".main-image img").click(function () {
+        $(".main-image img").attr("src", "images/main-image1.jpg")
+    });
+
+});
